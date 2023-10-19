f5a40ec5591789879a9422f6223ac63316634a44
jason <jason2580134@outlook.com>
2023-10-18 22:30:39 -0700

    refactor sprite class

---
+++ Sprite.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+class RectSprite {
+
+public:
+
+  RectSprite(float spriteWidth, sf::Vector2f spritePosition);
+  ~RectSprite();
+
+  sf::RectangleShape getSprite();
+
+  void moveSprite(sf::Vector2f spriteOffset);
+
+  void moveSprite(float xOffset, float yOffset);
+
+  void setSpritePosition(sf::Vector2f spriteOffset);
+
+  void createSpriteTexture(string path) {
+
+private:
+  void move(float xOffset, float yOffset);
+
+private:
+
+  sf::RectangleShape sprite;
+  sf::Texture spriteTexture;
+
+}
